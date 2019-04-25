4b4dddf626a3273e3c46174beeb9ce8996aba7f9
wellingtonx <wellingtonex@gmail.com>
2019-04-24 21:32:44 -0300

    configurando para rodar com o mysql

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,7 +1,7 @@
-DROP TABLE autobot IF EXISTS;
-
-CREATE TABLE autobot  (
-    autobot_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
-    name VARCHAR(50),
-    car VARCHAR(50)
-);
+DROP TABLE IF EXISTS autobot;
+CREATE TABLE `autobot` (
+  `autobot_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `car` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`autobot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
